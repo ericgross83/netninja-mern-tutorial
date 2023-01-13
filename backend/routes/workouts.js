281ef2f187de4811eaf68bb475245e3b50602928
Eric Gross <eric.gross@volkswagen.de>
2023-01-13 17:49:17 +0100

    Connect post Method to Mongo DB

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const Workout = require('../models/WorkoutModel')
 // GET all workouts
 router.get('/', (req, res) => {
     res.json({mssg: 'GET all workouts'})
@@ -13,8 +13,14 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
-    res.json({mssg: 'Workout posted'})
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch (e) {
+        res.status(400).json({error: e.message})
+    }
 })
 
 // DELETE a single workout
