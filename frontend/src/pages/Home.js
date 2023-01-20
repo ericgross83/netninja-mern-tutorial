cc262995a16aaa75bc1a614357b92402843e0625
Eric Gross <eric.gross@volkswagen.de>
2023-01-20 17:25:00 +0100

    Add fetching from backend server

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function Home() {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        async function fetchWorkouts() {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if(response.ok)
+            {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts()
+    },[])
+
     return (
         <div className={'home'}>
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => <div key={workout._id}>{workout.title}</div>
+                )}
+            </div>
         </div>
     );
 };
