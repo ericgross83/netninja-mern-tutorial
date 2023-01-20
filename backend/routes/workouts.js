f2f60e32c7d78048d97f50e8b9e081a8c90fdba7
Eric Gross <eric.gross@volkswagen.de>
2023-01-20 15:12:02 +0100

    Chapter 7

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,27 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const Workout = require('../models/WorkoutModel')
+const {createWorkout, getWorkout, getWorkouts} = require('../controllers/workoutController')
+
 // GET all workouts
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all workouts'})
-    
-})
+router.get('/', getWorkouts)
 
 // GET a single workout
-router.get('/:id', (req, res) => {
-    res.json({msg: 'GET a single workout'})
-})
+router.get('/:id', getWorkout)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-    const {title, load, reps} = req.body
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch (e) {
-        res.status(400).json({error: e.message})
-    }
-})
+router.post('/', createWorkout)
 
 // DELETE a single workout
 router.delete('/:id', (req, res) => {
